c319f71608634916c830ec2693aa959ec4a0ef70
Swan Yi Aung <114377846+seekerz12@users.noreply.github.com>
2026-07-27 23:10:57 +0700

    1st commit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,43 @@
+import React from 'react';
 import UserRegistration from './UserRegistration';
 
-function App() {
+export default function App() {
   return (
-    <div>
-      <UserRegistration />
+    <div style={styles.container}>
+      {/* Header Section matching image_a138d7.png */}
+      <header style={styles.header}>
+        <h1 style={styles.title}>React_Form_Handling</h1>
+        <hr style={styles.divider} />
+      </header>
+
+      {/* Main Content where the form will be displayed */}
+      <main>
+        <UserRegistration />
+      </main>
     </div>
   );
 }
 
-export default App;
+const styles = {
+  container: {
+    fontFamily: 'sans-serif',
+    padding: '20px',
+  },
+  header: {
+    maxWidth: '1000px',
+    margin: '0 auto',
+    paddingTop: '20px',
+  },
+  title: {
+    fontSize: '2rem',
+    fontWeight: 'normal',
+    color: '#333',
+    margin: '0 0 10px 0',
+    textAlign: 'center', // Or 'left' depending on your exact page width preferences
+  },
+  divider: {
+    border: 'none',
+    borderBottom: '1px solid #eaeaea',
+    margin: '0 0 30px 0',
+  }
+};
