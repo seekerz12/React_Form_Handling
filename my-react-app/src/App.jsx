e2611ef99dcf244758fb0f173553167b6cc7150b
Swan Yi Aung <114377846+seekerz12@users.noreply.github.com>
2026-07-27 23:27:27 +0700

    first commit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -6,14 +6,11 @@ export default function App() {
   return (
     <div style={{ fontFamily: 'sans-serif', padding: '20px' }}>
       <header style={{ maxWidth: '1000px', margin: '0 auto', paddingTop: '20px' }}>
-        <h1 style={{ fontSize: '2rem', fontWeight: 'normal', color: '#333', margin: '0 0 10px 0', textAlign: 'center' }}>
-          React_Form_Handling
-        </h1>
         <hr style={{ border: 'none', borderBottom: '1px solid #eaeaea', margin: '0 0 30px 0' }} />
       </header>
 
       <main>
-        {/* 2. Make sure this component is placed here! */}
+        
         <UserRegistration />
       </main>
     </div>
